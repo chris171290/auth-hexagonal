855ad1319af3fd5df498cfb270864b3bad8a1c5d
Christian Santanas <50605403+chris171290@users.noreply.github.com>
2024-10-31 00:01:08 -0400

    fix(auth): :bug: update infra user entity

--- src/context/auth/infrastructure/TypeOrm/TypeOrmUserEntity.ts
+++ src/context/auth/infrastructure/TypeOrm/TypeOrmUserEntity.ts
@@ -1,13 +1,27 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('users')
 export class TypeOrmUserEntity {
-  @PrimaryGeneratedColumn()
-  id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ unique: true })
+  email!: string;
 
   @Column()
-  email: string;
+  password!: string;
 
-  @Column()
-  password: string;
+  @Column({ nullable: true })
+  firstName!: string;
+
+  @Column({ nullable: true })
+  lastName: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
 }
